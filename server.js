9d1d106c47344d255210194f7ce7fc582cd117be
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-30 10:17:16 +0200

    testing

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/', (req, res) => {
+  fs.readFile('style.css', (err, data) => {
+    res.setHeader('Content-Type', 'text/style');
+    res.send(data);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
